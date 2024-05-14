1f4d74716868210ac73517b25a768698832ee683
Roger(Xingyun) Chen <44486666+chemxy@users.noreply.github.com>
2024-05-13 23:15:12 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
-import {useEffect, useState} from "react";
-import type {Schema} from "../amplify/data/resource";
-import {generateClient} from "aws-amplify/data";
 import {signInWithRedirect} from 'aws-amplify/auth';
 
-const client = generateClient<Schema>();
 
 function App() {
-    const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-    useEffect(() => {
-        client.models.Todo.observeQuery().subscribe({
-            next: (data) => setTodos([...data.items]),
-        });
-    }, []);
-
-    function createTodo() {
-        client.models.Todo.create({content: window.prompt("Todo content")});
-    }
 
     async function signInWithGoogle() {
         console.log("open with google");
